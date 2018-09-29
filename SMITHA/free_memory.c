3c90fc610adf2dbc97ef696721aba20c3e332fdd
SMITHABHASKAR <smbh4256@colorado.edu>
2018-09-29 16:00:56 -0600

    Addding the free_memory.c file

---
+++ SMITHA/free_memory.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include</home/smitha/PES_Project1/free_memory.h>
+#include</home/smitha/PES_Project1/allocate.h>
+
+
+
+int main(){
+
+printf("You have entered free memory command");
+
+if(&address!=0){
+free(address);
+printf("The data in address has been cleared \n ");
+}
+else {
+printf("No memory has been allocated to be freed \n ");
+printf("Go back to the command window to invoke the allocate memory command \n");
+
+}
+
+return 0;
+
+}
