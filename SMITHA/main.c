3c9fc876bd0d1ba1d4f03680fb41f88a04a2cb05
SMITHABHASKAR <smbh4256@colorado.edu>
2018-09-29 15:10:49 -0600

    Added the main file

---
+++ SMITHA/main.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include"/home/smitha/PES_Project1/help.h"
+
+char  func_comp(const void *a, const void *b);
+
+struct funcinvoke {
+char opcode;
+void (*func) ();
+};
+
+struct funcinvoke func[]={
+{'H' , help()},
+{'M' , write_memory()},
+{'F' , free_memory()},
+{'D' , display_memory()},
+{'W' , writeto_memory()},
+{'I' , invert_memory()}
+};
+
+//struct funcinvoke *fi =bsearch(func,6,sizeof(struct), func_compare);
+
+char  func_comp(const void *a , const void *b){
+const struct funcinvoke *p1=a , *p2=b ;
+return ((p1->opcode)-(p2->opcode));
+};
+
+int main() {
+char   entry;
+char *f;
+printf("Enter the opcode to invoke the appropriate function");
+scanf("%c",&entry);
+
+f=(char *) bsearch(&entry,func,6,sizeof(char),func_comp);
+struct funcinvoke a={entry , f};
+
+return 0;
+
+
+
+}
+
+
