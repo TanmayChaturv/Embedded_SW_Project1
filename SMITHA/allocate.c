2eb529fdc0b425071cb18bc7e4942805a7396f0a
SMITHABHASKAR <smbh4256@colorado.edu>
2018-09-29 15:22:56 -0600

    Adding the allocate.c and allocate.h file

---
+++ SMITHA/allocate.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include</home/smitha/PES_Project1/allocate.h>
+void print();
+int *allocate(int number);
+
+
+void print(){
+printf("You have entered the command to allocate memory");
+printf("Enter the number of 32-bit words you would like to allocate \n ");
+}
+
+int *allocate(int number){
+
+int *p;
+
+p=(int*)malloc(number * 4); //32 bit=4 bytes;
+
+printf("%d 32-bits have been successfully allocated /n",number);
+printf("The base-address of the allocated memory is %p /n",p);
+return p;
+}
+
+int main(){
+
+int number;
+int *a;
+
+print();
+scanf("%d",&number);
+
+a=allocate(number);
+
+return 0;
+
+}
