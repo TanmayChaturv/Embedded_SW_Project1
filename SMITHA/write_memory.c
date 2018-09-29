6f99bea38496a85c8ab03a2349ecf190a3e5efd1
SMITHABHASKAR <smbh4256@colorado.edu>
2018-09-29 17:53:55 -0600

    write_memory files need to be updated

---
+++ SMITHA/write_memory.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include</home/smitha/PES_Project1/allocate.h>
+
+void main(){
+__uint32_t* user_address;
+__uint32_t value;
+
+printf("Enter the address of the memory location you wish to update \n");
+scanf("%p",&user_address);
+
+printf("Enter the value you wish to write into address \n");
+scanf("%ls",&value);
+
+*((volatile int *)user_address)=value;
+
+printf("The updated value at %ls is %d",user_address,value);
+
+
+
+}
