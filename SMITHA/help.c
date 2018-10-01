c52931829d2a346d557a73e1dc6226c2e346e1ea
TanmayChaturv <tanmay.chaturvedi@colorado.edu>
2018-10-01 12:20:24 -0600

    help.c added

---
+++ SMITHA/help.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"help.h"
+//#include"main.h"
+
+void main()
+{
+	printf("Please Enter the following commands to invoke respective functions\n");
+	printf("'A' for allocation memory in a specified location \n");
+        printf("'F' for freeing up allocated memory \n");
+        printf("'D' for displaying contents in a specified area\n");
+        printf("'W' for writing to a specified memory area\n");
+        printf("'I' for inverting contents in a specified  memory area\n");
+        printf("'P' for generating a random pattern\n");
+        printf("'V' for verifying a previously generated random pattern\n");
+
+exit(0);
+
+}
+
--- SMITHA/invertbits.c
+++ SMITHA/invertbits.c
@@ -3,13 +3,13 @@
 #include"exec_time.h"
 
 
-
+exec_time(0); //Clock Start//
 void invert(uint32_t n_bits, uint32_t* st_addr )
 {
-	uint32_t n_bits; // number of bits//
-	uint32_t * st_addr; // start address //
-/* Need to check if start and end address lies /
-in the range of preallocated addresses, else allocate first*/
+	//uint32_t n_bits; // number of bits//
+	//uint32_t * st_addr; // start address //
+/*** Need to check if start and end address lies /
+in the range of preallocated addresses, else allocate first***/
 
 	uint32_t * en_addr = *st_addr + n_bits - 1; // end address = start + number of other addresses
 	uint32_t inverted_data;
@@ -20,10 +20,9 @@ for (int i=0; i<n_bits; i++)
 {
 	*(st_addr + i-1) = (*st_addr + i-1) ^ 0xFFFFFFFF; //Replaced original data with inverted data
 	//Skip printf statement to reduce execution time.
-
 }
-
-
+	double end_time = exec_time(1);
+	printf("time in micro seconds = %f", end_time*1000000);
 }
 
 
