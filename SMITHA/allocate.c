dcb039673a5b1737bbc8c8d642aa7dad658a0f8f
TanmayChaturv <tanmay.chaturvedi@colorado.edu>
2018-10-02 02:28:59 -0600

    Modified allocate.c,invertbits.c,write_memory.c, added global (extern) vars

--- SMITHA/allocate.c
+++ SMITHA/allocate.c
@@ -31,18 +31,21 @@ scanf("%d",&n_bits);
 
 st_addr=(uint32_t*)malloc(n_bits * 4); //32 bit=4 bytes;
 
-if (n_bits ){
-	if (n_bits >=2){
-		printf("'%d' 32-bits memory locations have been successfully allocated \n",n_bits);
-		printf("The start-address of the allocated memory is %p, and location index is %d \n",st_addr,st_index ); //gives address range to the user//
-		printf("The end-address of the allocated memory is %p, and location index is %d \n",st_addr +n_bits, en_index + n_bits ); 
-		return *st_addr;
-			}
-	else {
-		printf("'%d' 32-bits memory locations have been successfully allocated\n",n_bits);
-                printf("The start-address of the allocated memory is %p, and location index is %d \n",st_addr, st_index);
-	     }
-	}  
-else      printf("Please enter a number more than 0\n");
+if (st_addr != NULL){	
+	if (n_bits ){
+		if (n_bits >=2){
+			printf("'%d' 32-bits memory locations have been successfully allocated \n",n_bits);
+			printf("The start-address of the allocated memory is %p, and location index is %d \n",st_addr,st_index ); //gives address range to the user//
+			printf("The end-address of the allocated memory is %p, and location index is %d \n",st_addr +n_bits, en_index + n_bits ); 
+			return *st_addr;
+				}
+		else {
+			printf("'%d' 32-bits memory locations have been successfully allocated\n",n_bits);
+                	printf("The start-address of the allocated memory is %p, and location index is %d \n",st_addr, st_index);
+		     }
+		}  
+	else printf("Please enter a number more than 0\n");
+else printf("Null pointer returned, no memory allocated");
+}
 
 }
