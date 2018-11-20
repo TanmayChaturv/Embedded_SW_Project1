922f3cc27a1af2c5fcabe4bb562737852fc2806d
TanmayChaturvedi1 <tanmay.chaturvedi@colorado.edu>
2018-11-20 01:22:12 -0700

    Indentation improved

--- PROJECT 2- CIRCULAR BUFFER AND UART/rx_poll.c
+++ PROJECT 2- CIRCULAR BUFFER AND UART/rx_poll.c
@@ -35,15 +35,15 @@ void uart_init_rx(void)
 	SIM->SCGC4	|=	__UART0_CLK_EN_;	/*Enable Clk for UART0*/
 	SIM->SOPT2	|=	__CLK_SRC_FLLCLK_;	/*Select Clk source*/
 	UART0->C2	=	__UART0_DISBL_;		/*Disabling UART0 before config*/
-	UART0->BDH	=	0x00;				/*One stop bit, rest modulo divisor in UART0->BDL*/
-	UART0->BDL	=	0x19;				/*Setting BDL = 25, to be used with OSR = 15 to get 115200 bps with 0.16% precision calculated*/
-	UART0->C4	=	0xF;				/*Baudrate = (Clk Freq)/((OSR + 1)*SBR).. means 41.94Mhz/((15+1)*23)*/
+	UART0->BDH	=	0x00;			/*One stop bit, rest modulo divisor in UART0->BDL*/
+	UART0->BDL	=	0x19;			/*Setting BDL = 25, to be used with OSR = 15 to get 115200 bps with 0.16% precision calculated*/
+	UART0->C4	=	0xF;			/*Baudrate = (Clk Freq)/((OSR + 1)*SBR).. means 41.94Mhz/((15+1)*23)*/
 	UART0->C1	=	__UART0_8BIT_;		/*8-Bit mode*/
 	UART0->C1	=	__UART0_NO_PRTY_;	/*Parity disabled*/
 	UART0->C2	|=	__UART0_RXPOLL_EN_;	/*Receiver Enable Polling*/
 	SIM->SCGC5	|=	__PORTA_CLK_EN_;	/*Clk enable for PORTA*/
 	PORTA->PCR[2]	=	__PORTA_CLK_EN_;	/*MUX; Set to Alternative 2 for UART0*/
-	delay();							/*Delay for configurations to get stable*/
+	delay();						/*Delay for configurations to get stable*/
 }
 
 /*Monitor Status Reg 1, bit RDRF and poll if rxbuf empty*/
--- PROJECT 2- CIRCULAR BUFFER AND UART/tx_poll.c
+++ PROJECT 2- CIRCULAR BUFFER AND UART/tx_poll.c
@@ -25,16 +25,16 @@ void tx_poll(char *str)
 
 void uart_init_tx(void)
 {
-	SIM->SCGC4		|=	__UART0_CLK_EN_;	/*Enable Clk for UART0*/
-	SIM->SOPT2		|=	__CLK_SRC_FLLCLK_;	/*Select Clk source*/
-	UART0->C2		=	__UART0_DISBL_;		/*Disabling UART0 before config*/
-	UART0->BDH		=	0x00;			/*One stop bit, rest modulo divisor in UART0->BDL*/
-	UART0->BDL		=	0x19;			/*Setting BDL = 25, to be used with OSR = 15 to get 115200 bps with 0.16% precision calculated*/
-	UART0->C4		=	0xF;			/*Baudrate = (Clk Freq)/((OSR + 1)*SBR).. means 48Mhz/((15+1)*25)*/
-	UART0->C1		=	__UART0_8BIT_;		/*8-Bit mode*/
-	UART0->C1		=	__UART0_NO_PRTY_;	/*Parity disabled*/
-	UART0->C2		|=	__UART0_TXPOLL_EN_;	/*Transmitter Enable Polling*/
-	SIM->SCGC5		|=	__PORTA_CLK_EN_;	/*Clk enable for PORTA*/
+	SIM->SCGC4	|=	__UART0_CLK_EN_;	/*Enable Clk for UART0*/
+	SIM->SOPT2	|=	__CLK_SRC_FLLCLK_;	/*Select Clk source*/
+	UART0->C2	=	__UART0_DISBL_;		/*Disabling UART0 before config*/
+	UART0->BDH	=	0x00;			/*One stop bit, rest modulo divisor in UART0->BDL*/
+	UART0->BDL	=	0x19;			/*Setting BDL = 25, to be used with OSR = 15 to get 115200 bps with 0.16% precision calculated*/
+	UART0->C4	=	0xF;			/*Baudrate = (Clk Freq)/((OSR + 1)*SBR).. means 48Mhz/((15+1)*25)*/
+	UART0->C1	=	__UART0_8BIT_;		/*8-Bit mode*/
+	UART0->C1	=	__UART0_NO_PRTY_;	/*Parity disabled*/
+	UART0->C2	|=	__UART0_TXPOLL_EN_;	/*Transmitter Enable Polling*/
+	SIM->SCGC5	|=	__PORTA_CLK_EN_;	/*Clk enable for PORTA*/
 	PORTA->PCR[2]	=	__PORTA_MUX_UART0_;	/*MUX; Set to Alternative 2 for UART0*/
 	delay();						/*Delay for configurations to get stable*/
 }
