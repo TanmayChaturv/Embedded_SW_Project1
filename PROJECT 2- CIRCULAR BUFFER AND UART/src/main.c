4b45b4db4c63dbe7ab233ad9e3dd03f01880c985
Tanmay Chaturvedi <43152963+TanmayChaturvedi1@users.noreply.github.com>
2018-11-28 03:06:17 -0700

    Add files via upload

--- PROJECT 2- CIRCULAR BUFFER AND UART/src/main.c
+++ PROJECT 2- CIRCULAR BUFFER AND UART/src/main.c
@@ -5,13 +5,14 @@ circbuf_t *TXbuf;
 circbuf_t *RXbuf;
 extern uint32_t char_database[255];
 int n = 50;	/*change the number of fibonacci numbers*/
-int fibo(int);	
-int f;	/*Fibonacci index*/
+int fibo(int);
+
 
 int main(void)
 {
 	__disable_irq();
 	__enable_irq();
+	int f= 0;	/*Fibonacci index*/
 
 	TXbuf = (circbuf_t*)malloc(sizeof(circbuf_t));
 	status TXbuf_stat = buff_init(TXbuf, 255);
@@ -27,8 +28,9 @@ int main(void)
 	{
 	if(rx_flag == 1)	/*This flag is set when first interrupt happens. Cleared when below report is printed*/
 	{
-		for(int i = 0; i <= 100; i++);	/*wait for loop to complete before printing any report*/
+		//for(int i = 0; i <= 100; i++);	/*wait for loop to complete before printing any report*/
 		char str[30] = {0};
+		char str_fibo[30] = {0};
  	   	tx_poll("\n\r*Report:*\n\r");
  	   	for(int i = 0; i< 255; i++)
 		{
@@ -36,21 +38,26 @@ int main(void)
 			{
  				sprintf(str, "\n\r %c - %lu\n\r", i, char_database[i]);
  		   		tx_poll(str);
-				str[30]={0};
-				sprintf(str, "\n\r Fibonacci Value = %d\n\r",fibo(f));
-				tx_poll(str);
-                        }
+			//	str[30]={0};
+
+            }
 		}
 		rx_flag = 0;
+		int fibo_value = fibo(f);
+ 	    sprintf(str_fibo, "\n\r Fibonacci Value = %d\n\r",fibo_value);
+ 	   	tx_poll(str_fibo);
+
 	 }
 	else
 	{
-		int f = 0,
-		int  c;
+		//int f = 0;
+		int c;
 		for (c = 1; c <= n; c++)
       		{
+			//fibo(f);
 			f++;
       		}
+		f=0;
 	}
 	}
 }
