ed32c569d689fa47c565f59112e8ac51a1dc1d5f
SMITHABHASKAR <smbh4256@colorado.edu>
2018-12-06 13:31:34 -0700

    Added the adc.c and adc.h file

---
+++ PROJECT 3- ADC,DMA, DSP/adc.h
@@ -0,0 +1,15 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: Smitha Bhaskar and Tanmay Chaturvedi
+
+ 		Description:
+ 		This is the header file for the ADC code file. 
+ */
+
+#ifndef SOURCES_ADC_H_
+#define SOURCES_ADC_H_
+void adc_init(void);
+unsigned short adc_read();
+#endif /* SOURCES_ADC_H_ */
