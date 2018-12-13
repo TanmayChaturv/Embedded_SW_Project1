fb20f2f0a1f9d1ac2d0919c24577109ed7525ffc
SMITHA BHASKAR <smbh4256@colorado.edu>
2018-12-13 10:33:42 -0700

    Add files via upload

--- PROJECT 3- ADC,DMA, DSP/SRC/dma_final.c
+++ PROJECT 3- ADC,DMA, DSP/SRC/dma_final.c
@@ -1,6 +1,5 @@
 /*
  * dma_final.c
-
  *
  *  Created on: Dec 12, 2018
  *      Author: Smitha Bhaskar & Tanmay Chaturvedi
@@ -12,15 +11,23 @@
 #include "MKL25Z4.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 
-int16_t buff;
-int16_t buff_dma;
+int16_t buff[64];
+int16_t buff_dma[64];
+
 
 /* Function Prototypes */
 void DMA_Configure(void);
-void DMA_Init(void);
+int16_t DMA_Init(void);
+void GPIO_Init(void);
 
+
+void GPIO_Init(void){
+	PORTE_PCR30 |= PORT_PCR_MUX(1);
+	GPIOE_PDDR |= (1<<30);
+}
 
 
 void DMA_Configure(void)
@@ -40,21 +47,31 @@ void DMA_Configure(void)
 
 }
 
-void DMA_Init(void)
+int16_t DMA_Init(void)
 {
+	char str[20]={0};
+	adc_init();
+	GPIO_Init();
+	for(int i=0;i<64;i++)
+	{
+		buff[i]=adc_read();
 
-	   adc_init();
-	   buff=adc_read();
 
 		/*DMA_SAR(0) 	= &buff	;
 		DMA_DAR(0)	= &buff_dma;*/
 		DMA_DCR0 |= (DMA_DCR_DSIZE(0) | DMA_DCR_DINC(1) |
-						 	 DMA_DCR_SSIZE(0) | DMA_DCR_SINC(1));
+						 	 DMA_DCR_SSIZE(0) | DMA_DCR_SINC(0) | DMA_DCR_EINT(1));
 
 		//		 Source address reg
-				DMA_SAR0 = DMA_SAR_SAR(&buff);
+				DMA_SAR0 = DMA_SAR_SAR(&buff[i]);
+
+				uart_print(str);
+
 		//		 Destination address reg
-				DMA_DAR0 = DMA_DAR_DAR(&buff_dma);
+				DMA_DAR0 = DMA_DAR_DAR(&buff_dma[i]);
+
+				uart_print(str);
+
 
 			//	 Byte control register - total no of bytes to be transfered
 				DMA_DSR_BCR0 = DMA_DSR_BCR_BCR(64);
@@ -62,26 +79,30 @@ void DMA_Init(void)
 			//	 Start DMA
 				DMA_DCR0 |= DMA_DCR_START(1);
 
+				if(abs(buff_dma[i])>max)
+					max=abs(buff_dma[i]);
 
 
+
+		}
+		return max;
 }
 
 
 void DMA0_IRQHandler()
 {
+
+
 	__disable_irq();
+	GPIOE_PSOR |=(1<<30);
 	if(DMA_DSR_BCR0 & DMA_DSR_BCR_DONE_MASK)
 		{
 
-		char str[15]={0};
-
-				sprintf(str,"\n\r DMA= %d",*(&buff_dma+1));
-				uart_print(str);
 			/* Clear the interrupt source */
 			DMA_DSR_BCR0 |= DMA_DSR_BCR_DONE(1);
 
 		}
+	GPIOE_PCOR |=(1<<30);
 	__enable_irq();
+
 }
-
-
